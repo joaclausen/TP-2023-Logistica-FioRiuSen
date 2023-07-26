ae16206b60e4b23a462e36a2d3fc1aebfa422026
zapamaster <juane.fiorio99@gmail.com>
2023-07-25 21:16:58 -0300

    ESTO ES UNA PRUEBA

---
+++ src/main/java/com/mycompany/tp/logistica/fioriusen/entidades/TESTCLASSFORCOMMITINGPROJECT.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.tp.logistica.fioriusen.entidades;
+
+/**
+ *
+ * @author zapam
+ */
+public class TESTCLASSFORCOMMITINGPROJECT {
+    //el primero que me ve es gei
+}
